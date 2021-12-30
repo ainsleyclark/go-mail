f050a58120148401274cd8dff21bc7c6543de360
Ainsley Clark <info@ainsleyclark.com>
2021-12-30 11:58:24 +0000

    Added goreleaser

---
+++ bin/tag.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# tag.sh
+#
+
+# Set variables
+version=$1
+message=$2
+
+# Check goreleaser passed
+goreleaser check
+
+# Check version is not empty
+if [[ $version == "" ]]
+  then
+    echo "Add Version number"
+    exit
+fi
+
+# Check commit message is not empty
+if [[ $message == "" ]]
+  then
+    echo "Add commit message"
+    exit
+fi
+
+echo "Releasing version: " $version
+
+git tag -a "$version" -m "$message"
+git push origin $version
